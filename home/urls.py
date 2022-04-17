a799c21785d8db1d675c3fac97c4bb8bb0a07bc9
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-04-17 16:29:39 +0600

    Production

--- home/urls.py
+++ home/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-   path('', views.home, name='home'),
+   path('', views.index, name='index'),
    path('userlogin/', views.userlogin, name='userlogin'),
    path('testlogin/', views.testlogin, name='testlogin'),
    path('signup/', views.signup, name='signup'),
    path('userlogout/', views.userlogout, name='userlogout'),
    path('spots/',views.spots, name='spots'),
-   path('index/',views.index, name='index'),
+   path('home/',views.home, name='home'),
 ]
