e9a960c0cb22f313590a7e9c62b586ba8acc7398
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-04-17 17:08:46 +0600

    heroku fix

--- ParkingManagementSystem/settings.py
+++ ParkingManagementSystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ri59oolqamb1qh$3yhbrr0jq&&aoz=bz@n#!zb4fslla2ss+v2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tanjimcse370.herokuapp.com']
+ALLOWED_HOSTS = ['tanjimparking.herokuapp.com','https://tanjimparking.herokuapp.com/']
 
 
 # Application definition
@@ -142,4 +142,3 @@ AUTH_USER_MODEL = 'home.Users'
 #     'django.contrib.auth.backends.ModelBackend',
 # )
 
-django_heroku.settings(locals())
