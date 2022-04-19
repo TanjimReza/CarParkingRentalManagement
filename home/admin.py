a28e3de66c67275a2b31c6660b6a9bb2ef6993e3
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-04-20 01:20:52 +0600

    Index Page Done

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-from home.models import Users
 from django.contrib import admin
 from django.contrib.auth.models import User
 # Register your models here.
@@ -8,7 +7,5 @@ admin.site.register(Users)
 admin.site.register(ParkingSlots)
 admin.site.register(SpotOwner)
 admin.site.register(Rentee)
-admin.site.register(Engine)
-admin.site.register(FuelType)
-# admin.site.register(Payment)
+admin.site.register(Payment)
 admin.site.register(Rentee_Reviews_ParkingSlots)
