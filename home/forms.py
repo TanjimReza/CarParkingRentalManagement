728e405247aa363a10830b366cb6b681d5ab5d53
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2022-04-20 01:23:48 +0600

    Index Page Done

--- home/forms.py
+++ home/forms.py
@@ -15,16 +15,7 @@ class RenteeForm(ModelForm):
     class Meta:
         model = Rentee
         fields = '__all__'
-
-class EngineForm(ModelForm):
-    class Meta:
-        model = Engine
-        fields = '__all__'
         
-class FuelTypeForm(ModelForm):
-    class Meta:
-        model = FuelType
-        fields = '__all__'
 class PaymentForm(ModelForm):
     class Meta:
         model = Payment
